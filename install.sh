1cd64d6b82d4da1f2576794b4690cb1e1c2916c5
Andrew <zezba9000@gmail.com>
2025-01-06 21:16:49 -0800

    Remove unused config settings

--- install.sh
+++ install.sh
@@ -64,73 +64,10 @@ fi
 
 echo "Updating Menu-Button systemd config"
 bash -c 'cat > /etc/systemd/hwdb.d/60-keyboard.hwdb << '\''EOF'\''
-###########################################################
-# MSI (aka "Micro Star")
-###########################################################
-
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMICRO-STAR*:pn*:*
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMicro-Star*:pn*:*
- KEYBOARD_KEY_76=f21                                    # Toggle touchpad, sends meta+ctrl+toggle
- KEYBOARD_KEY_91=config                                 # MSIControl Center
- KEYBOARD_KEY_a0=mute                                   # Fn+F9
- KEYBOARD_KEY_ae=volumedown                             # Fn+F7
- KEYBOARD_KEY_b0=volumeup                               # Fn+F8
- KEYBOARD_KEY_b2=www                                    # e button
- KEYBOARD_KEY_c2=ejectcd
- KEYBOARD_KEY_df=sleep                                  # Fn+F12
- KEYBOARD_KEY_e2=bluetooth                              # satellite dish2
- KEYBOARD_KEY_e4=f21                                    # Fn+F3 Touchpad disable
- KEYBOARD_KEY_ec=email                                  # envelope button
- KEYBOARD_KEY_ee=camera                                 # Fn+F6 camera disable
- KEYBOARD_KEY_f1=f20                                    # Microphone mute
- KEYBOARD_KEY_f2=rotate_display                         # Rotate screen
- KEYBOARD_KEY_f6=wlan                                   # satellite dish1
- KEYBOARD_KEY_f7=brightnessdown                         # Fn+F4
- KEYBOARD_KEY_f8=brightnessup                           # Fn+F5
- KEYBOARD_KEY_f9=search
-
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMICRO-STAR*:pnGE60*:*
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMICRO-STAR*:pnGE70*:*
- KEYBOARD_KEY_c2=ejectcd
-
-# some MSI models generate ACPI/input events on the LNXVIDEO input devices,
-# plus some extra synthesized ones on atkbd as an echo of actually changing the
-# brightness; so ignore those atkbd ones, to avoid loops
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMICRO-STAR*:pn*U-100*:*
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMICRO-STAR*:pn*U100*:*
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMICRO-STAR*:pn*N033:*
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMicro-Star*:pn*VR420*:*
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMicro-Star*:pn*PR200*:*
- KEYBOARD_KEY_f7=reserved
- KEYBOARD_KEY_f8=reserved
-
-# MSI Wind U90/U100 generates separate touchpad on/off keycodes so ignore touchpad toggle keycode.
-# Also ignore Wi-Fi and Bluetooth keycodes, because they are generated when the HW rfkill state
-# changes, but the userspace also toggles the SW rfkill upon receiving these keycodes.
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMICRO-STAR*:pnU90/U100:*
- KEYBOARD_KEY_e4=unknown
- KEYBOARD_KEY_e2=unknown
- KEYBOARD_KEY_f6=unknown
-
-# Keymaps MSI Prestige And MSI Modern FnKeys and Special keys
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMicro-Star*:pn*Prestige*:*
-evdev:atkbd:dmi:bvn*:bvr*:bd*:svnMicro-Star*:pn*Modern*:*
- KEYBOARD_KEY_91=prog1                                  # Fn+F7 Creation Center, sometime F7
- KEYBOARD_KEY_f2=prog2                                  # Fn+F12 Screen rotation
- KEYBOARD_KEY_8d=prog3                                  # Fn+A Change True Color selections
- KEYBOARD_KEY_8c=prog4                                  # Fn+Z Launch True Color
- KEYBOARD_KEY_f5=fn_esc                                 # Fn+esc Toggle the behaviour of Fn keys
- KEYBOARD_KEY_97=unknown                                # Lid close
- KEYBOARD_KEY_98=unknown                                # Lid open
-
-evdev:name:MSI Laptop hotkeys:dmi:bvn*:bvr*:bd*:svn*:pnM[iI][cC][rR][oO]-S[tT][aA][rR]*:*
- KEYBOARD_KEY_0213=f22
- KEYBOARD_KEY_0214=f23
-
 # MSI Claw
 evdev:name:AT Translated Set 2 keyboard:dmi:*:svnMicro-StarInternationalCo.,Ltd.:pnClawA1M:*
- KEYBOARD_KEY_b9=f15                                   # Right Face Button
- KEYBOARD_KEY_ba=f16                                   # Left Face Button
+ KEYBOARD_KEY_b9=f15  #Right Button
+ KEYBOARD_KEY_ba=f16  #Left Button
 EOF'
 
 # install HID trigger service
